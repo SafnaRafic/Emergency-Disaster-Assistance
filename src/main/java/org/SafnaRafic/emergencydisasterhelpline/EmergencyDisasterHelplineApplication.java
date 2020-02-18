dd79906173b99e7b54881659f8fe7598661ae399
Safna <safna.usa@gmail.com>
2020-02-18 00:30:33 -0600

    Initial commit

---
+++ src/main/java/org/SafnaRafic/emergencydisasterhelpline/EmergencyDisasterHelplineApplication.java
@@ -0,0 +1,13 @@
+package org.SafnaRafic.emergencydisasterhelpline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyDisasterHelplineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyDisasterHelplineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/SafnaRafic/emergencydisasterhelpline/EmergencyDisasterHelplineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.SafnaRafic.emergencydisasterhelpline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyDisasterHelplineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
